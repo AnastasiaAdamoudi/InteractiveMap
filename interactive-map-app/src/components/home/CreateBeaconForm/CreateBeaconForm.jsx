7dc9e724d4e43411a347facbafb2cab29c6bc5c9
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-12-15 22:41:49 +0000

    Uninstalled unused packages

--- interactive-map-app/src/components/home/CreateBeaconForm/CreateBeaconForm.jsx
+++ interactive-map-app/src/components/home/CreateBeaconForm/CreateBeaconForm.jsx
@@ -1,16 +1,25 @@
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 import "./CreateBeaconForm.css";
-import axios from 'axios';
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
-const CreateBeaconForm = ({ isOpen, onClose, beacons, updateBeacons, beaconArrayLength }) => {
-
+const CreateBeaconForm = ({
+  isOpen,
+  onClose,
+  beacons,
+  updateBeacons,
+  beaconArrayLength,
+}) => {
   const closeIcon = (
     <svg fill="#6227a1" viewBox="0 0 40 40" width={40} height={40}>
-      <path d="M 10,10 L 30,30 M 30,10 L 10,30" stroke="purple" strokeWidth="3" />
+      <path
+        d="M 10,10 L 30,30 M 30,10 L 10,30"
+        stroke="purple"
+        strokeWidth="3"
+      />
     </svg>
   );
 
@@ -18,23 +27,31 @@ const CreateBeaconForm = ({ isOpen, onClose, beacons, updateBeacons, beaconArray
     creatorName: z.string().nonempty({ message: "Creator name is required" }),
     creatorEmail: z.string().email({ message: "Invalid email address" }),
     beaconName: z.string().nonempty({ message: "Beacon name is required" }),
-    beaconLocation: z.string().nonempty({ message: "Beacon location is required" }),
-    beaconLatitude: z.string().nonempty({ message: "Beacon latitude is required" }),
-    beaconLongitude: z.string().nonempty({ message: "Beacon longitude is required" }),
-    beaconDescription: z.string().nonempty({ message: "Beacon description is required" }),
+    beaconLocation: z
+      .string()
+      .nonempty({ message: "Beacon location is required" }),
+    beaconLatitude: z
+      .string()
+      .nonempty({ message: "Beacon latitude is required" }),
+    beaconLongitude: z
+      .string()
+      .nonempty({ message: "Beacon longitude is required" }),
+    beaconDescription: z
+      .string()
+      .nonempty({ message: "Beacon description is required" }),
     beaconUrl: z.string(),
   });
 
-  const { register, handleSubmit, formState, reset } = useForm({ resolver: zodResolver(schema) });
+  const { register, handleSubmit, formState, reset } = useForm({
+    resolver: zodResolver(schema),
+  });
 
   const { errors } = formState;
 
-
-    const onSubmit = async (formData) => {
-      try {
-
+  const onSubmit = async (formData) => {
+    try {
       const newBeacon = {
-        number: (beaconArrayLength + 1),
+        number: beaconArrayLength + 1,
         creatorName: formData.creatorName,
         creatorEmail: formData.creatorEmail,
         beaconName: formData.beaconName,
@@ -53,95 +70,152 @@ const CreateBeaconForm = ({ isOpen, onClose, beacons, updateBeacons, beaconArray
 
       updateBeacons(newBeacon);
 
-      reset(
-        {
-          creatorName: "",
-          creatorEmail: "",
-          beaconName: "",
-          beaconLocation: "",
-          beaconLatitude: "",
-          beaconLongitude: "",
-          beaconDescription: "",
-          beaconUrl: "",
-        }
-      );
+      reset({
+        creatorName: "",
+        creatorEmail: "",
+        beaconName: "",
+        beaconLocation: "",
+        beaconLatitude: "",
+        beaconLongitude: "",
+        beaconDescription: "",
+        beaconUrl: "",
+      });
 
-    onClose();
-
+      onClose();
     } catch (err) {
       console.error(err.message);
     }
-  }
+  };
 
   return (
     <Modal
       open={isOpen}
-      onClose={() => { onClose()
-      reset()}
-      }
+      onClose={() => {
+        onClose();
+        reset();
+      }}
       center
       closeIcon={closeIcon}
       classNames={{
-        overlay: 'customOverlay',
-        modal: 'customModal',
+        overlay: "customOverlay",
+        modal: "customModal",
       }}
     >
       <div className="text-popups-container">
         <div className="text-popups-text">
-        <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="creatorName">Creator Name</label>
+              <input
+                {...register("creatorName", { required: true })}
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter your name"
+              />
+              <div style={{ color: "red" }}>{errors?.creatorName?.message}</div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="creatorName">Creator Name</label>
-    <input {...register("creatorName", { required: true })} type="text" className="form-control" id="name" placeholder="Enter your name" />
-    <div style={{ color: "red" }}>{errors?.creatorName?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="creatorEmail">Creator Email</label>
+              <input
+                {...register("creatorEmail", { required: true })}
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email"
+              />
+              <div style={{ color: "red" }}>
+                {errors?.creatorEmail?.message}
+              </div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="creatorEmail">Creator Email</label>
-    <input {...register("creatorEmail", { required: true })} type="email" className="form-control" id="email" placeholder="Enter your email" />
-    <div style={{ color: "red" }}>{errors?.creatorEmail?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconName">Beacon Name</label>
+              <input
+                {...register("beaconName", { required: true })}
+                type="text"
+                className="form-control"
+                id="beaconName"
+                placeholder="Enter beacon name"
+              />
+              <div style={{ color: "red" }}>{errors?.beaconName?.message}</div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconName">Beacon Name</label>
-    <input {...register("beaconName", { required: true })} type="text" className="form-control" id="beaconName" placeholder="Enter beacon name" />
-    <div style={{ color: "red" }}>{errors?.beaconName?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconLocation">Beacon Location</label>
+              <input
+                {...register("beaconLocation", { required: true })}
+                type="text"
+                className="form-control"
+                id="beaconLocation"
+                placeholder="Enter beacon location"
+              />
+              <div style={{ color: "red" }}>
+                {errors?.beaconLocation?.message}
+              </div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconLocation">Beacon Location</label>
-    <input {...register("beaconLocation", { required: true })} type="text" className="form-control" id="beaconLocation" placeholder="Enter beacon location" />
-    <div style={{ color: "red" }}>{errors?.beaconLocation?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconDescription">Beacon Description</label>
+              <input
+                {...register("beaconDescription", { required: true })}
+                type="text"
+                className="form-control"
+                id="beaconDescription"
+                placeholder="Enter beacon description"
+              />
+              <div style={{ color: "red" }}>
+                {errors?.beaconDescription?.message}
+              </div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconDescription">Beacon Description</label>
-    <input {...register("beaconDescription", { required: true })} type="text" className="form-control" id="beaconDescription" placeholder="Enter beacon description" />
-    <div style={{ color: "red" }}>{errors?.beaconDescription?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconLatitude">Beacon Latitude</label>
+              <input
+                {...register("beaconLatitude", { required: true })}
+                type="number"
+                step="any"
+                className="form-control"
+                id="beaconLatitude"
+                placeholder="Enter beacon latitude"
+              />
+              <div style={{ color: "red" }}>
+                {errors?.beaconLatitude?.message}
+              </div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconLatitude">Beacon Latitude</label>
-    <input {...register("beaconLatitude", { required: true })} type="number" step="any" className="form-control" id="beaconLatitude" placeholder="Enter beacon latitude" />
-    <div style={{ color: "red" }}>{errors?.beaconLatitude?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconLongitude">Beacon Longitude</label>
+              <input
+                {...register("beaconLongitude", { required: true })}
+                type="number"
+                step="any"
+                className="form-control"
+                id="beaconLongitude"
+                placeholder="Enter beacon longitude"
+              />
+              <div style={{ color: "red" }}>
+                {errors?.beaconLongitude?.message}
+              </div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconLongitude">Beacon Longitude</label>
-    <input {...register("beaconLongitude", { required: true })} type="number" step="any" className="form-control" id="beaconLongitude" placeholder="Enter beacon longitude" />
-    <div style={{ color: "red" }}>{errors?.beaconLongitude?.message}</div>
-</div>
+            <div className="form-group">
+              <label htmlFor="beaconUrl">Beacon URL</label>
+              <input
+                {...register("beaconUrl")}
+                type="text"
+                className="form-control"
+                id="beaconUrl"
+                placeholder="Enter beacon URL"
+              />
+              <div style={{ color: "red" }}>{errors?.beaconUrl?.message}</div>
+            </div>
 
-<div className="form-group">
-    <label htmlFor="beaconUrl">Beacon URL</label>
-    <input {...register("beaconUrl")} type="text" className="form-control" id="beaconUrl" placeholder="Enter beacon URL" />
-    <div style={{ color: "red" }}>{errors?.beaconUrl?.message}</div>
-</div>
-
-<button type="submit" className="btn btn-primary">
-    Submit
-</button>
-</form>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </Modal>
--- interactive-map-app/src/components/home/CreateBeaconForm/index.js
+++ interactive-map-app/src/components/home/CreateBeaconForm/index.js
@@ -1,4 +1,3 @@
 import CreateBeaconForm from './CreateBeaconForm';
-import SubmitButton from './SubmitButton';
 
 export { CreateBeaconForm };
