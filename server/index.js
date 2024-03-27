ecc1cda9f74f6580a207682cc00d4b9e921d574f
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-03-28 00:25:42 +0200

    Initialized Node project, created server, connected server to mongodb database, prepared folders and files for models, routes, controllers

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import { beaconsRouter } from "./routes/routes.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.use("/beacons", beaconsRouter);
