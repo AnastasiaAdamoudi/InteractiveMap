c419efbb0466603a55ffb6a13854351db4ae3cf0
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-19 20:40:41 +0100

    Updated vite config with vite plugin external

--- interactive-map-app/vite.config.js
+++ interactive-map-app/vite.config.js
@@ -11,30 +11,42 @@
 //   }
 // });
 
+// import { defineConfig } from 'vite';
+// import importToCDN from 'vite-plugin-cdn-import'; // Importing importToCDN from the package
+
+// export default defineConfig({
+//     plugins: [
+//         importToCDN({
+//             modules: [
+//                 {
+//                     name: 'react',
+//                     var: 'React',
+//                     path: `umd/react.production.min.js`,
+//                 },
+//                 {
+//                     name: 'react-dom',
+//                     var: 'ReactDOM',
+//                     path: `umd/react-dom.production.min.js`,
+//                 },
+//             ],
+//         }),
+//     ],
+//     build: {
+//         rollupOptions: {
+//             external: ['@hookform/resolvers/zod'],
+//         },
+//     },
+// });
+
 import { defineConfig } from 'vite';
-import importToCDN from 'vite-plugin-cdn-import'; // Importing importToCDN from the package
+import external from 'vite-plugin-external';
 
 export default defineConfig({
-    plugins: [
-        importToCDN({
-            modules: [
-                {
-                    name: 'react',
-                    var: 'React',
-                    path: `umd/react.production.min.js`,
-                },
-                {
-                    name: 'react-dom',
-                    var: 'ReactDOM',
-                    path: `umd/react-dom.production.min.js`,
-                },
-            ],
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            external: ['@hookform/resolvers/zod'],
-        },
-    },
+  plugins: [
+    external({
+      react: 'https://unpkg.com/react@18/umd/react.production.min.js',
+      'react-dom': 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    }),
+  ],
 });
 
