4d44d30c7aaef83d1d0a3caa5bac803b5aa26125
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-19 20:31:00 +0100

    Updated vite config file

--- interactive-map-app/vite.config.js
+++ interactive-map-app/vite.config.js
@@ -1,12 +1,38 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@hookform/resolvers/zod', 'react', 'react-dom', 'leaflet', 'react-leaflet']
-    }
-  }
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['@hookform/resolvers/zod', 'react', 'react-dom', 'leaflet', 'react-leaflet']
+//     }
+//   }
+// });
+
+import importToCDN from 'vite-plugin-cdn-import'
+
+export default {
+    plugins: [
+        importToCDN({
+            modules: [
+                {
+                    name: 'react',
+                    var: 'React',
+                    path: `umd/react.production.min.js`,
+                },
+                {
+                    name: 'react-dom',
+                    var: 'ReactDOM',
+                    path: `umd/react-dom.production.min.js`,
+                },
+            ],
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            external: ['@hookform/resolvers/zod'],
+        },
+    },
+}
