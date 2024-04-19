45ed5317425cb280ea52ee61e0896b0cc8116d8f
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-19 09:48:18 +0100

    Fixed imports in Home

--- interactive-map-app/src/pages/Home.jsx
+++ interactive-map-app/src/pages/Home.jsx
@@ -1,14 +1,21 @@
-import MainMap from '../components/home/Map/index.js';
+import { MainMap } from '../components/home/Map/index.js';
 import { CreateBeaconButton } from '../components/home/HomeButtons/index.js';
 import './Home.css';
+import PropTypes from 'prop-types';
 
-const Home = ({ formOpen, setFormOpen, closeForm, beacons, updateBeacons }) => {
+const Home = ({ formOpen, setFormOpen, closeForm, beacons }) => {
+
+Home.propTypes = {
+  formOpen: PropTypes.bool.isRequired,
+  setFormOpen: PropTypes.func.isRequired,
+  closeForm: PropTypes.func.isRequired,
+  beacons: PropTypes.array.isRequired,
+};
 
   return (
     <div className="home">
         <MainMap beacons={beacons} />
-        <CreateBeaconButton formOpen={formOpen} setFormOpen={setFormOpen} closeForm={closeForm} beacons={beacons}
-        updateBeacons={updateBeacons} />
+        <CreateBeaconButton formOpen={formOpen} setFormOpen={setFormOpen} closeForm={closeForm} beacons={beacons} />
     </div>
   )
 }
