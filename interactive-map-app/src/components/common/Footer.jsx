9214ca5f920fa042f258015690a9fcefd57a6ab7
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-22 11:42:33 +0100

    Edited Footer styling

--- interactive-map-app/src/components/common/Footer.jsx
+++ interactive-map-app/src/components/common/Footer.jsx
@@ -21,32 +21,12 @@ const Footer = ({ footerModals, setFooterModals }) => {
   return (
     <div className="footer-container">
 
-<div className="copyright-container">
+<div className="left-side-container">
+<div>
         <p className="footer-text">
           © 2024 <a href="https://campfireconvention.network/" alt="Campfire Convention" target="_blank" rel="noreferrer" className="text-link">
           Campfire Convention</a>
         </p>
-      </div>
-
-      <div className="join-button-container">
-      <a href="https://campfireconvention.network/" alt="Campfire Convention" target="_blank" rel="noreferrer" className="text-link">
-        <button className="join-button">Join the Campfire Convention</button>
-      </a>
-      </div>
-
-      <div className="credits-popupbuttons-container">
-        <div className="credits-container">
-          <p className="footer-text">
-            Developed by{" "}
-            <a
-              href="https://www.anastasiaadamoudi.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-link"
-            >
-              Anastasia
-            </a>, Jon & Tim
-          </p>
         </div>
         <div className="popup-buttons-container">
           <p type="button" onClick={togglePrivacyModal} className="text-link">
@@ -59,6 +39,34 @@ const Footer = ({ footerModals, setFooterModals }) => {
             className="text-link"
           >
             Web Accessibility Statement
+          </p>
+        </div>
+      </div>
+
+      <div className="middle-container">
+      <a href="https://campfireconvention.network/" alt="Campfire Convention" target="_blank" rel="noreferrer" className="text-link">
+        <button className="join-button">Join the Campfire Convention</button>
+      </a>
+      </div>
+
+      <div className="right-side-container">
+        <div className="credits-container">
+          <p className="footer-text">
+            Developed by{" "}
+            <a
+              href="https://www.anastasiaadamoudi.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-link"
+            >
+              Anastasia
+            </a>
+          </p>
+          </div>
+          <div>
+          <p className="footer-text">
+          With the support of{" "}
+          Jon & Tim 
           </p>
         </div>
       </div>
